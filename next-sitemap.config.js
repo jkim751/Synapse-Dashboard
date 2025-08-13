e61acd7627feab7fbca0facddf4d96bd06584ebd
jkim751 <kimjason751@gmail.com>
2025-08-14 06:07:12 +1000

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://your-domain-name.com', // IMPORTANT: Replace with your actual domain
+    siteUrl: 'https://www.synapseeducation-dashboard.com.au/', // IMPORTANT: Replace with your actual domain
     generateRobotsTxt: true, // (Optional) This will also create a robots.txt file
     // ...other options
   }
